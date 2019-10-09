4b5bc9a61143499c4fe1fc4807a803c4e7c5b97b
Ikram Hossen <dcseikramhossen1392@gmail.com>
2019-10-09 03:41:15 -0700

    simple student crud operation

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Students Records</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
